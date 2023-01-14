acb70455011bb0c3fca91d5f289e5235596cb948
Nakka Nithish <nakkanithish27@gmail.com>
2023-01-14 19:08:16 +0530

    initial commit

---
+++ comments.js
@@ -0,0 +1,3 @@
+//name the images as your convinience
+//random number and image name are linked,so the image src change based on the random number,thus changing image
+//we compare the two random numbers to find the winner
